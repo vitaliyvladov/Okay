2171b5c2bf3e0399cb88e6e28296762123e1b66f
mt1sk <mt1sk@mail.ru>
2018-02-01 18:28:03 +0200

    migrations_moved_renamed. Миграции бд перенесены в файл migrate.php, и удалены все не нужные скрипты. Изменены пути sql файлов миграций.

--- migration/migrate.php
+++ migration/migrate.php
@@ -6,18 +6,42 @@
 */
 
 define("ROOT_DIR", dirname(__DIR__)."/");
-define("MIGRATION_DIR", ROOT_DIR."migration/");
+define("MIGRATIONS_DIR", ROOT_DIR."migration/migrations/");
 chdir(ROOT_DIR);
 
 $action = !empty($argv[1]) ? $argv[1] : "update";
 switch ($action) {
     case 'update': {
-        require_once(MIGRATION_DIR."update_database.php");
+        require_once("api/Okay.php");
+        $okay = new Okay();
+
+        $okay->db->query("SHOW TABLES LIKE 'mi_executed_migration'");
+        $exist = $okay->db->results();
+        $already_executed = array();
+        if (!empty($exist)) {
+            $okay->db->query("SELECT `name` FROM mi_executed_migration");
+            $already_executed = $okay->db->results('name');
+        }
+
+        foreach (glob(MIGRATIONS_DIR."*.up.sql") as $path) {
+            $file = pathinfo($path, PATHINFO_BASENAME);
+            if (!in_array($file, $already_executed)) {
+                $errors = $okay->db->restore($path, true);
+                $okay->db->query("INSERT INTO mi_executed_migration SET `name`=?", $file);
+                echo $file."-----".(!empty($errors) > 0 ? "ERROR" : "OK").PHP_EOL;
+                foreach ($errors as $error) {
+                    echo $error.PHP_EOL;
+                }
+                if (!empty($errors)) {
+                    echo PHP_EOL;
+                }
+            }
+        }
         break;
     }
     case 'create': {
         $name = date("YmdHis").(empty($argv[2]) ? '' : '_'.$argv[2]).".up.sql";
-        fclose(fopen(MIGRATION_DIR."sql/".$name, "w"));
+        fclose(fopen(MIGRATIONS_DIR.$name, "w"));
         break;
     }
     default: {
--- migration/migrate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# запускать с командной строки в текущей папке: sh migrate.sh
-#php ./migrate.php
-php ${PWD}/migrate.php $1 $2
